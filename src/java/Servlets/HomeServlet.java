3f6d1867728e2ec9d3cc00af5a88374b1bbb9659
xbali <xbali@DESKTOP-8J9OVMN>
2023-02-06 09:48:02 -0700

    Created the Servlets NetBeans project that has a Login and Logout Functionality

---
+++ src/java/Servlets/HomeServlet.java
@@ -0,0 +1,31 @@
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author xbali
+ */
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
